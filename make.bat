dc07dee4d0ea25d45a1f4eab77570065c0afd330
home.nhat.nguyenhong <hongnhatbkhn@gmail.com>
2018-01-05 00:39:07 +0700

    - update build system

--- make.bat
+++ make.bat
@@ -3,7 +3,7 @@
 pushd %~d0
 
 set msvcdir="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\"
-if not defined DevEnvDir call %msvcdir%vcvars64.bat >nul
+if not defined DevEnvDir call %msvcdir%vcvars32.bat >nul
 
 popd %~d0
 
@@ -18,7 +18,7 @@ rem /Fd		target file output path
 rem /Zi		generate .pdb
 rem /Od		disable optimization (Debug)
 rem --------------------------------------------
-echo Compiling...
+echo Compiling helich...
 call cl.exe										^
 	/nologo										^
 	/I "."										^
@@ -36,7 +36,7 @@ call cl.exe										^
 rem --------------------------------------------
 rem Linking
 rem --------------------------------------------
-echo Linking...
+echo Linking helich...
 call lib.exe									^
 	/nologo										^
 	build/obj/unitybuild_0.obj					^
